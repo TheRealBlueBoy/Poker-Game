6cbcbf9b84ad664e978e22948c7c20af4cdc8903
TheRealBlueBoy <73229087+TheRealBlueBoy@users.noreply.github.com>
2024-01-26 12:50:15 +0100

    structure

--- Scripts/GameMode.gd
+++ Scripts/GameMode.gd
@@ -1,11 +1,39 @@
-extends Node
+class_name GameMode
+var cardDeck
+var players
+var betPerPlayer
+var bigBlind
+var smallBlind
+var actingPlayer
 
-
+enum PlayerStatus {pending, raised, folded, broke, allIn}
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass # 
+func Setup():
+	pass
+func SetupTable():
+	pass
+func SetupPlayers():
+	pass
+func SetupDeck():
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#gamecycle
+func StartTurn():
+	pass
+func EndTurn():
+	pass
+func StartRound():
+	pass
+func EndRound():
+	pass
+func RevealCard():
+	pass
+func DecideWinner():
+	pass
+func DealCards():
+	pass
+func ShuffleDeck():
 	pass
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,18 @@
-extends Node
+class_name Player
+var hand
+var playerIdx
+var playerName
+var chipsOwned
+var Status
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	pass 
+func Pass():
+	pass
+func Call():
+	pass
+func Check():
+	pass
+func Fold():
 	pass
